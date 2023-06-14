5e947c11cbe984de278faa4df7076ce605751fdb
vuk-david <vukdavid@gmail.com>
2023-06-14 20:33:40 +0200

    Remove old GUI python files & Add .ui files & Add switching screens

---
+++ src/gui/main_gui.py
@@ -0,0 +1,134 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
+from PyQt5.QtCore import Qt
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("Main Window Title")  # Set the title for the main window
+
+        ui_file = os.path.join(script_dir, "test.ui")
+        loadUi(ui_file, self)
+
+        # Keys
+        self.actionGenerate_new_Key.triggered.connect(self.goToGenerateNewKey)
+        # self.actionRemove_existing_Key.triggered.connect(self.goToRemoveExistingKey)
+        # self.actionImport_Key_2.triggered.connect(self.goToImportKey)
+        # self.actionExport_Key_2.triggered.connect(self.goToExportKey)
+        # self.actionShow_Keyrings.triggered.connect(self.goToShowKeyrings)
+
+        # Messages
+        self.actionSend_Message.triggered.connect(self.goToSendMessage)
+        # self.actionReceive_Message.triggered.connect(self.goToReceiveMessage)
+
+    def goToGenerateNewKey(self):
+        generateNewKey = GenerateNewKey()
+        widget.addWidget(generateNewKey)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def goToSendMessage(self):
+        sendMessage = SendMessage()
+        widget.addWidget(sendMessage)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class GenerateNewKey(QDialog):
+    def __init__(self):
+        super(GenerateNewKey, self).__init__()
+
+        ui_file = os.path.join(script_dir, "generateNewKey.ui")
+        loadUi(ui_file, self)
+
+        self.UiComponents()
+
+    # Method for widgets
+    def UiComponents(self):
+        self.backButton.clicked.connect(self.back)
+        self.generateButton.clicked.connect(self.goToSavePrivateKey)
+
+    def goToSavePrivateKey(self):
+        savePrivateKey = SavePrivateKey()
+        widget.addWidget(savePrivateKey)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    # Action method
+    def back(self):
+        # Get the current index of the widget you want to remove
+        current_index = widget.currentIndex()
+
+        # Remove the widget at the current index
+        widget.removeWidget(widget.widget(current_index))
+
+
+
+class SavePrivateKey(QDialog):
+    def __init__(self):
+        super(SavePrivateKey, self).__init__()
+
+        ui_file = os.path.join(script_dir, "savePrivateKey.ui")
+        loadUi(ui_file, self)
+
+        self.UiComponents()
+
+    # Method for widgets
+    def UiComponents(self):
+        self.backButton.clicked.connect(self.back)
+        self.saveButton.clicked.connect(self.getBackToMainWindow)
+
+
+    # Action method
+    def getBackToMainWindow(self):
+        current_index = widget.currentIndex()
+        widget.removeWidget(widget.widget(current_index))
+        current_index = widget.currentIndex()
+        widget.removeWidget(widget.widget(current_index))
+
+    def back(self):
+        current_index = widget.currentIndex()
+        widget.removeWidget(widget.widget(current_index))
+
+
+class SendMessage(QDialog):
+    def __init__(self):
+        super(SendMessage, self).__init__()
+        
+        ui_file = os.path.join(script_dir, "savePrivateKey.ui")
+        loadUi(ui_file, self)
+        
+        self.UiComponents()
+
+    # Method for widgets
+    def UiComponents(self):
+        self.backButton.clicked.connect(self.back)
+
+
+    # Action method
+    def back(self):
+        current_index = widget.currentIndex()
+        widget.removeWidget(widget.widget(current_index))
+
+
+# main
+app = QApplication(sys.argv)
+
+app.setApplicationDisplayName("PGP Mitic-Davidovic")
+
+widget = QtWidgets.QStackedWidget()
+mainWindow = MainWindow()
+
+widget.addWidget(mainWindow)
+widget.setFixedHeight(350)
+widget.setFixedWidth(600)
+widget.show()
+mainWindow.show()
+
+try:
+    sys.exit(app.exec_())
+
+except:
+    print("Exiting")
