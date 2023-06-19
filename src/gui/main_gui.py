93e703b6322c4e36b866650562152f95b0fea4ac
Nikola Mitić <101733519+miticn@users.noreply.github.com>
2023-06-19 05:10:34 +0200

    Signing vorks

--- src/gui/main_gui.py
+++ src/gui/main_gui.py
@@ -345,6 +345,8 @@ class SendMessage(QDialog):
             if not sender_key.checkPassword(password):
                 self.errorLabel.setText("Wrong password")
                 return
+            
+            print(sender_key)
 
             
 
